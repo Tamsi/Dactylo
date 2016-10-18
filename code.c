ed0c6a0934f5f41e941e7ab6b1fb137b01e1a8cb
tamsi.besson@epitech.eu <tamsi.besson@epitech.eu>
2016-10-18 14:27:39 +0200

    includes

--- code.c
+++ code.c
@@ -5,8 +5,9 @@
 ** Login   <Tamsi@epitech.net>
 ** 
 ** Started on  Tue Oct 18 14:24:01 2016 Tamsi Besson
-** Last update Tue Oct 18 14:24:49 2016 Tamsi Besson
+** Last update Tue Oct 18 14:27:18 2016 Tamsi Besson
 */
+#include <stdio.h>
 
 char	*str_to_cmp()
 {
--- main.c
+++ main.c
@@ -5,8 +5,9 @@
 ** Login   <Tamsi@epitech.net>
 ** 
 ** Started on  Tue Oct 18 14:24:54 2016 Tamsi Besson
-** Last update Tue Oct 18 14:25:04 2016 Tamsi Besson
+** Last update Tue Oct 18 14:27:33 2016 Tamsi Besson
 */
+#include <stdio.h>
 
 int	main()
 {

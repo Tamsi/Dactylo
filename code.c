03f8d256f489c57ee9dd4fcfb158cca597d75c7f
tamsi.besson@epitech.eu <tamsi.besson@epitech.eu>
2016-10-18 14:25:13 +0200

    commit

---
+++ code.c
@@ -0,0 +1,19 @@
+/*
+** code.c for  in /home/Tamsi/Dactylo
+** 
+** Made by Tamsi Besson
+** Login   <Tamsi@epitech.net>
+** 
+** Started on  Tue Oct 18 14:24:01 2016 Tamsi Besson
+** Last update Tue Oct 18 14:24:49 2016 Tamsi Besson
+*/
+
+char	*str_to_cmp()
+{
+  
+}
+
+char	*str()
+{
+  
+}
---
+++ main.c
@@ -0,0 +1,14 @@
+/*
+** main.c for  in /home/Tamsi/Dactylo
+** 
+** Made by Tamsi Besson
+** Login   <Tamsi@epitech.net>
+** 
+** Started on  Tue Oct 18 14:24:54 2016 Tamsi Besson
+** Last update Tue Oct 18 14:25:04 2016 Tamsi Besson
+*/
+
+int	main()
+{
+  
+}

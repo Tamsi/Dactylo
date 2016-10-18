8041b231a4845d39741eafebbe4cb3799f65fbe3
tamsi.besson@epitech.eu <tamsi.besson@epitech.eu>
2016-10-18 14:39:47 +0200

    commit

--- code.c
+++ code.c
@@ -5,16 +5,23 @@
 ** Login   <Tamsi@epitech.net>
 ** 
 ** Started on  Tue Oct 18 14:24:01 2016 Tamsi Besson
-** Last update Tue Oct 18 14:27:18 2016 Tamsi Besson
+** Last update Tue Oct 18 14:39:42 2016 Tamsi Besson
 */
 #include <stdio.h>
 
 char	*str_to_cmp()
 {
-  
+  char	*str_fix;
+
+  str_fix = "cours de dactylo";
+  printf("%s\n", str_fix);
+  return (str_fix);
 }
 
-char	*str()
+char	*my_str()
 {
-  
+  char	*str_write;
+
+  scanf("%s", str_write);
+  my_str_cmp(str_to_cmp, str_write);
 }
